c247f3d6248f4eb891fe2b0ff26e602eaf128c90
Supta Richard Philip <supta.philip@gmail.com>
2020-02-04 22:38:16 +0600

    String Example

---
+++ StringExample.java
@@ -0,0 +1,15 @@
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		
+		
+		String txt = "Hello World";
+		System.out.println("The length of txt: " + txt.length());
+		
+		System.out.println("Upper case"+txt.toUpperCase());
+		System.out.println("Lower case"+txt.toLowerCase());   
+		
+	}
+
+}
