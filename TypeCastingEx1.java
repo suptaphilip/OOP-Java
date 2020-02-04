40e89df6aea1ce781d5efb2969864361799cdba9
Supta Richard Philip <supta.philip@gmail.com>
2020-02-04 20:53:16 +0600

    TypeCasting

---
+++ TypeCastingEx1.java
@@ -0,0 +1,17 @@
+
+public class TypeCastingEx1{  
+   public static void main(String args[]){  
+	//double value
+	double dnum = 99.99;
+	//convert double to int using typecasting
+	int inum=(int)dnum;  
+	//displaying int value after conversion
+	System.out.println(inum);  
+	
+	int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+    System.out.println(myDouble); 
+	
+	
+   }
+}
